09d7f895ac277abf26d1fa6ee9b4fb725b766314
root <jhheidrich@gmail.com>
2024-03-12 13:44:56 +0000

    initial commit

---
+++ atualizador.ps1
@@ -0,0 +1,77 @@
+# É usado o comando Get-SmbOpenFile para verificar os arquivos abertos no smb e o comando Close-SmbOpenFile para encerrar
+# É filtrada apenas as linhas que contém a string com o nome do sistema no ShareRelativePath
+#
+# Ex de uso: atualizador.ps1 -sistema imoveis -namezip imoveis2024.01.26.0
+# Sendo windows, foi considerada a possibilidade de haver sistema aberto no servidor também, por isso os comandos 
+# Get-Process e Stop-Process para encerrar processos
+#
+# Também é verificado por arquivos na pasta do sistema sendo usados por processos
+# Os handles em uso por esse processo na pasta são fechados
+#
+# Como extra o processo é mostrado na tela
+#
+
+param (
+$sistema = $(throw "-sistema parameter is required. Ex: atualizador.ps1 -sistema imoveis -namezip imoveis2024.01.26.0"),
+$namezip = $(throw "-namezip parameter is required. Ex: atualizador.ps1 -sistema imoveis -namezip imoveis2024.01.26.0")
+)
+$scdir = split-path -parent $MyInvocation.MyCommand.Definition
+$skydir = Split-Path -Parent $scdir
+$fname = (Get-ChildItem -Path $skydir -Filter "$namezip.zip" -Recurse).DirectoryName
+$BKPPATH=$env:Path
+$env:Path = $env:Path+";$scdir"
+
+#echo "scdir $scdir"; echo "sistema $sistema"; echo "namezip $namezip"; echo "skydir $skydir"; echo "fname $fname";echo "PATH $env:Path"#; exit
+
+
+function closeof {
+    Write-Host Conferindo arquivos abertos no compartilhamento
+    foreach ($openfile in Get-SmbOpenFile |Where-Object ShareRelativePath -like "*$sistema*"|Select-Object FileId,Path) {
+        Write-Host Ecerrando $openfile.Path
+        Close-SmbOpenFile -Force $openfile.FileId
+        }
+}
+
+function closeop {
+    Write-Host Conferindo processos locais abertos...
+    foreach ($openprocess in Get-Process |Where-Object ProcessName -Like "$sistema*"|Select-Object Id,ProcessName) {
+        Write-Host Ecerrando $openprocess.ProcessName
+        Stop-Process -Force -Id $openprocess.Id
+        }
+}
+
+function closelof {
+    rm -Force $scdir\handles.txt 2> $null
+    foreach ( $pids in .\handle64.exe -NoBanner $fname | ForEach-Object { $_.split(":")[1] }|ForEach-Object { $_.split(" ")[1] }|Sort-Object -Unique ) {
+        handle64.exe -NoBanner -p $pids | Select-String "$sistema"|Set-Content -Path $scdir\handles.txt
+        echo "Encerrando handles de arquivos abertos no processo de pid $pids"; handle64.exe -NoBanner -p $pids | Select-String "$sistema"
+        foreach ( $handles in Get-Content $scdir\handles.txt | ForEach-Object { $_.split(":")[0] } ) {
+            handle64.exe -nobanner -p $pids -c $handles -y >> $null            
+        }
+    }
+}
+
+function extnversion {
+    echo "Extraindo nova versão $namezip"
+    Expand-Archive -Verbose -Force -LiteralPath "$fname-/$namezip.zip" -DestinationPath "$fname-/"
+}
+
+
+# ----- INÍCIO -----
+cd $scdir
+
+closeof
+closeop
+
+if ( -not (handle64.exe -nobanner $fname).contains("No matching handles found")) {
+    closelof
+}
+
+echo "Renomeando pasta $fname para $fname-"
+mv $fname $fname-
+extnversion
+echo "Renomeando pasta $fname- para $fname"
+mv $fname- $fname
+
+$env:Path=$BKPPATH
+
